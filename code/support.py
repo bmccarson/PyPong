a2c5c9f7695018d09d17d6ae81d2091fa4c03274
bmccarson <brian@mccarson.us>
2024-08-24 14:25:09 -0400

    add support function file

---
+++ code/support.py
@@ -0,0 +1,7 @@
+from settings import *
+from os.path import join
+
+def import_image(*path, alpha = True, format = 'png'):
+	full_path = join(*path) + f'.{format}'
+	surf = pygame.image.load(full_path).convert_alpha() if alpha else pygame.image.load(full_path).convert()
+	return surf
