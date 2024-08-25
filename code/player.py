4275fef53a361d9e690622e039032d1b620905ea
bmccarson <brian@mccarson.us>
2024-08-25 11:57:16 -0400

    create parent paddle class

--- code/player.py
+++ code/player.py
@@ -1,6 +1,24 @@
+from pygame.sprite import _Group
 from settings import *
 from support import import_image
 
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, image):
+        super().__init__(groups)
+        self.image = image
+        self.rect = self.image.get_frect(center = pos)
+        self.direction = 0
+        self.old_rect = self.rect.copy()
+
+    def move(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+        self.rect.top = 0 if self.rect.top < 0 else self.rect.top
+        self.rect.bottom = WINDOW_HEIGHT if self.rect.bottom > WINDOW_HEIGHT else self.rect.bottom
+
+    def update(self, dt):
+        self.old_rect = self.rect.copy()
+        self.input()
+        self.move(dt)
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
@@ -10,7 +28,8 @@ class Player(pygame.sprite.Sprite):
         # movement
         self.direction = pygame.Vector2()
         self.speed = SPEED['player']
-        self.old_rect = self.rect.copy() 
+        self.old_rect = self.rect.copy()
+         
     
     def input(self):
         keys = pygame.key.get_pressed()
